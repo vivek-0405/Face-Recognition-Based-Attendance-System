ae66ca6b8de8efc15e9df9a094070c9f5e73fe17
Vivek <vivekkumr042005@gmail.com>
2026-06-23 16:13:33 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import pandas as pd
+import time
+from datetime import datetime
+from streamlit_autorefresh import st_autorefresh
+
+ts=time.time()
+date=datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
+timestamp=datetime.fromtimestamp(ts).strftime("%H:%M:%S")
+
+count=st_autorefresh(interval=2000,limit=100,key="fizzbuzzcounter")
+if count == 0:
+    st.write("Count is zero")
+elif count%3==0 and count%5==0:
+    st.write("FizzBuzz")
+elif count%3==0:
+    st.write("Fizz")
+elif count%5==0:
+    st.write("Buzz")
+else:
+    st.write(f"Count: {count}")
+
+df=pd.read_csv("Attendance/Attendance_"+date+".csv")
+st.dataframe(df)
